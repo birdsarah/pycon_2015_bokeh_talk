1b7939ca063992c42cb17298eb2769452f71e831
Sarah Bird <sarah@bonvaya.com>
2015-04-07 07:25:11 -0700

    Make static map work

--- washmap/washmap/washmap_static.py
+++ washmap/washmap/washmap_static.py
@@ -13,11 +13,14 @@ from .water_map import (
     construct_map,
     construct_line,
     construct_text_box,
+    construct_key,
     layout_components,
 )
 from .chart_constants import (
     BLUE,
-    GREEN
+    GREEN,
+    WATER_COLOR_RANGE,
+    SANITATION_COLOR_RANGE,
 )
 
 
@@ -31,41 +34,45 @@ def make_washmap():
     country = 'South Africa'
 
     wat_source = ColumnDataSource(wat_data)
-    wat_plot = construct_map(wat_source)
 
     wat_data_text = get_frame_for_country(wat_data, country)
     wat_source_text = ColumnDataSource(wat_data_text)
-    wat_text = construct_text_box(wat_source_text, bar_color=BLUE)
 
     year_range = [str(x) for x in range(1990, 2013)]
     wat_data_line = wat_data_text[year_range].transpose()
     wat_data_line.columns = ['value']
     wat_data_line = wat_data_line[wat_data_line['value'] > 0]
     wat_source_line = ColumnDataSource(wat_data_line)
+
+    wat_plot = construct_map(wat_source)
     wat_line = construct_line(wat_source_line)
+    wat_text = construct_text_box(wat_source_text, bar_color=BLUE)
+    wat_key = construct_key(WATER_COLOR_RANGE)
 
     san_source = ColumnDataSource(san_data)
-    san_plot = construct_map(san_source)
 
     san_data_text = get_frame_for_country(san_data, country)
     san_source_text = ColumnDataSource(san_data_text)
-    san_text = construct_text_box(san_source_text, bar_color=GREEN)
 
     san_data_line = san_data_text[year_range].transpose()
     san_data_line.columns = ['value']
     san_data_line = san_data_line[san_data_line['value'] > 0]
     san_source_line = ColumnDataSource(san_data_line)
+
+    san_plot = construct_map(san_source)
     san_line = construct_line(san_source_line, line_color=GREEN)
+    san_text = construct_text_box(san_source_text, bar_color=GREEN)
+    san_key = construct_key(SANITATION_COLOR_RANGE)
 
     tabs = Tabs(
         tabs=[
             Panel(
                 title="Water",
-                child=layout_components(wat_plot, wat_line, wat_text)
+                child=layout_components(wat_plot, wat_line, wat_text, wat_key)
             ),
             Panel(
                 title="Sanitation",
-                child=layout_components(san_plot, san_line, san_text)
+                child=layout_components(san_plot, san_line, san_text, san_key)
             )
         ]
     )
