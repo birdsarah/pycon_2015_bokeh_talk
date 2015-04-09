dc59d793e6e91fd868adc9e4a399a7d6a75880fc
Sarah Bird <sarah@bonvaya.com>
2015-04-09 02:40:21 -0400

    Turn off clickjacking protection
    so can embed views in presentation

--- washmap/main/settings.py
+++ washmap/main/settings.py
@@ -63,7 +63,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'main.urls'
