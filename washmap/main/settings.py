a883d7d793a752b3516f8166fe860608b5f1ffc2
Sarah Bird <sarah@bonvaya.com>
2015-04-17 01:38:56 -0400

    Add HTTP_X_FORWARDED

--- washmap/main/settings.py
+++ washmap/main/settings.py
@@ -38,6 +38,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 
 # Application definition
